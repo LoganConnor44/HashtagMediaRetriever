8342d6353c0ffc7203a817ee208c3a0b86d592fe
Logan Connor <loganconnor44@gmail>
2018-07-08 21:49:28 -0400

    changing property name

--- HashtagMediaRetriever/src/products/SocialMediaPlatform.cs
+++ HashtagMediaRetriever/src/products/SocialMediaPlatform.cs
@@ -7,7 +7,7 @@ namespace HashtagMediaRetriever.src.products {
     public abstract class SocialMediaPlatform {
 		protected string Name { get; set; }
 		protected string Hashtag { get; set; }
-		protected Dictionary<int, Media> Media { get; set; }
+		protected Dictionary<int, Memory> Media { get; set; }
 		protected ICredentials Authentication { get; set; }
         public List<Memento> Mementos { get; set; }
     }
